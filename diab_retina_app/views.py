6d9a3580785fb1a8fc77bfdf620fcd33f26e7c13
fassane <fassanebolly@gmail.com>
2023-01-23 07:29:03 +0000

    views

--- diab_retina_app/views.py
+++ diab_retina_app/views.py
@@ -9,8 +9,27 @@ from diab_retina_app import process
 @csrf_exempt
 def display(request):
     if request.method == 'GET':
-        return render(request, 'index.html')
+        return render(request, 'app.html')
 
+@csrf_exempt
+def home(request):
+    if request.method == 'GET':
+        return render(request, 'home.html')
+
+@csrf_exempt
+def about(request):
+    if request.method == 'GET':
+        return render(request, 'about.html')
+
+@csrf_exempt
+def fonction(request):
+    if request.method == 'GET':
+        return render(request, 'fonction.html')
+
+@csrf_exempt
+def demo(request):
+    if request.method == 'GET':
+        return render(request, 'demo.html')
 
 @csrf_exempt
 def process_image(request):
