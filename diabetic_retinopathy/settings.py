dc28536a45356e4a6c397139adfd0c45e62ab671
fassane <fassanebolly@gmail.com>
2023-01-23 07:30:02 +0000

    app settings

--- diabetic_retinopathy/settings.py
+++ diabetic_retinopathy/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/home/thealoneprogrammer/PycharmProjects/diabetic_retinopathy/diab_retina_app/output/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    STATIC_URL,
+    os.path.join(BASE_DIR, "static") 
+
 ]
